599380878e8f5700685a1185a61594fa36a73056
hakarnikiya2000 <hakarnikiya@gmail.com>
2024-11-10 22:28:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,27 +3,28 @@ const fs = require('fs-extra')
   
   
  //═══════[Required Variables]════════\\ 
- global.owner = process.env.OWNER_NUMBER.split(",") 
+ global.owner = process.env.OWNER_NUMBER.split("94740881677") 
  global.mongodb = process.env.MONGODB_URI || "mongodb+srv://vajira:vajira@cluster0.hzwqrqq.mongodb.net/?retryWrites=true&w=majority" 
  global.port= process.env.PORT || 5000 
  global.email = 'sam@secktor.live' 
  global.github = 'https://github.com/vajirabot1' 
  global.location = 'Sri lanka, Rathnapura' 
  global.gurl = 'https://instagram.com/' // add your username 
- global.sudo = process.env.SUDO || '919628516236' 
+ global.sudo = process.env.SUDO || '94771190955' 
  global.devs = '919628516236'; 
  global.website = 'https://github.com/' //wa.me/+91000000000000 
  global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/fb7993e24f7ecdaecbed5.jpg' 
  module.exports = { 
    botname:   process.env.BOT_NAME === undefined ? 'King-Vajira' : process.env.BOT_NAME, 
-   ownername: process.env.OWNER_NAME === undefined ? 'Vajira' : process.env.OWNER_NAME,  
+   ownername: process.env.OWNER_NAME === undefined ? 'Nikiya' : process.env.OWNER_NAME,  
    author:  process.env.PACK_INFO.split(";")[0] === undefined ? '' : process.env.PACK_INFO.split(";")[0], 
    auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS, 
    packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '-' : process.env.PACK_INFO.split(";")[1],
-   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID, 
+   sessionName:  process.env.SESSION_ID === undefined ? 'VAJIRA;;;SwordsDraped'
+ : process.env.SESSION_ID, 
    autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION , 
    antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD, 
-   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
 voice  :  process.env.AUTO_VOICE === undefined ? false : process.env.AUTO_VOICE, 
    antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE, 
    readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE, 
